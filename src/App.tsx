b384ce4b20666677c8b4c69312233c8af07686fb
Nick Gauthier <ngauthier404@icloud.com>
2023-10-25 17:04:30 -0400

    Update formatting of source code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { LoginButton } from './components/LoginButton';
-import './App.css';
+import React from "react";
+import { LoginButton } from "./components/LoginButton";
+import "./App.css";
 
 function App() {
   return (
