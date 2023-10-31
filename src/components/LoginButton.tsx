2f982fcb0a12dcbf16202541d009e1586d2960f3
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-10-31 15:48:03 -0400

    Link login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export function LoginButton(): JSX.Element {
   return (
     <span>
-      <Button
-        style={{
-          height: "50px",
-          width: "100px",
-          fontSize: "24px",
-          marginLeft: "900px",
-          marginBottom: "750px",
-        }}
-      >
-        Login
-      </Button>
+      <Link to={"/Login"}>
+        <Button
+          style={{
+            height: "50px",
+            width: "100px",
+            fontSize: "24px",
+            marginLeft: "900px",
+            marginBottom: "750px",
+          }}
+        >
+          Login
+        </Button>
+      </Link>
     </span>
   );
 }
