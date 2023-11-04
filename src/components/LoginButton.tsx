ce29ad284809fa887924b5f790d978872f79ecd7
Nick Gauthier <ngauthier404@icloud.com>
2023-11-04 10:31:37 -0400

    Update login link

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,21 +3,21 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export function LoginButton(): JSX.Element {
-  return (
-    <span>
-      <Link to={"/Login"}>
-        <Button
-          style={{
-            height: "50px",
-            width: "100px",
-            fontSize: "24px",
-            marginLeft: "900px",
-            marginBottom: "750px",
-          }}
-        >
-          Login
-        </Button>
-      </Link>
-    </span>
-  );
+    return (
+        <span>
+            <Link to={"/login"}>
+                <Button
+                    style={{
+                        height: "50px",
+                        width: "100px",
+                        fontSize: "24px",
+                        marginLeft: "900px",
+                        marginBottom: "750px",
+                    }}
+                >
+                    Login
+                </Button>
+            </Link>
+        </span>
+    );
 }
--- src/tests/login.test.tsx
+++ src/tests/login.test.tsx
@@ -5,53 +5,53 @@ import { LoginPage } from "../pages/LoginPage";
 import userEvent from "@testing-library/user-event";
 
 describe("Simple tests for login page", () => {
-  test("Login button is present", () => {
-    render(<App />);
+    test("Login button is present", () => {
+        render(<App />);
 
-    const loginButton = screen.getByRole("button", { name: "Login" });
+        const loginButton = screen.getByRole("button", { name: "Login" });
 
-    expect(loginButton).toBeInTheDocument();
-  });
-
-  test("Login button redirects to the login page", () => {
-    render(<App />);
-
-    const loginButton = screen.getByRole("button", { name: "Login" });
-    act(() => {
-      loginButton.click();
+        expect(loginButton).toBeInTheDocument();
     });
 
-    expect(document.location.pathname).toEqual("/Login");
-  });
+    test("Login button redirects to the login page", () => {
+        render(<App />);
 
-  test("Login page displays a form for email and password", () => {
-    render(<LoginPage />);
+        const loginButton = screen.getByRole("button", { name: "Login" });
+        act(() => {
+            loginButton.click();
+        });
 
-    const emailBox = screen.getByPlaceholderText("Email");
-    const passwordBox = screen.getByPlaceholderText("Password");
-
-    expect(emailBox).toBeInTheDocument();
-    expect(passwordBox).toBeInTheDocument();
-  });
-
-  test("Login page allows for inputting of both email and password", () => {
-    const [exampleEmail, examplePassword] = [
-      "example@example.com",
-      "examplePassword",
-    ];
-
-    render(<LoginPage />);
-
-    const emailBox = screen.getByPlaceholderText("Email");
-    const passwordBox = screen.getByPlaceholderText("Password");
-
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
-      userEvent.type(emailBox, exampleEmail);
-      userEvent.type(passwordBox, examplePassword);
+        expect(document.location.pathname).toEqual("/login");
     });
 
-    expect(emailBox).toHaveValue(exampleEmail);
-    expect(passwordBox).toHaveValue(examplePassword);
-  });
+    test("Login page displays a form for email and password", () => {
+        render(<LoginPage />);
+
+        const emailBox = screen.getByPlaceholderText("Email");
+        const passwordBox = screen.getByPlaceholderText("Password");
+
+        expect(emailBox).toBeInTheDocument();
+        expect(passwordBox).toBeInTheDocument();
+    });
+
+    test("Login page allows for inputting of both email and password", () => {
+        const [exampleEmail, examplePassword] = [
+            "example@example.com",
+            "examplePassword",
+        ];
+
+        render(<LoginPage />);
+
+        const emailBox = screen.getByPlaceholderText("Email");
+        const passwordBox = screen.getByPlaceholderText("Password");
+
+        // eslint-disable-next-line testing-library/no-unnecessary-act
+        act(() => {
+            userEvent.type(emailBox, exampleEmail);
+            userEvent.type(passwordBox, examplePassword);
+        });
+
+        expect(emailBox).toHaveValue(exampleEmail);
+        expect(passwordBox).toHaveValue(examplePassword);
+    });
 });
