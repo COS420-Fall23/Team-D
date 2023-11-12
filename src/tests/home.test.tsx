96ff674330b661b8cfff8e9dd70eccabad9e5636
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-11-12 16:04:34 -0500

    test CI intagration

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -2,24 +2,30 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 describe("Simple tests for the home page", () => {
-    beforeEach(() => {
-        // eslint-disable-next-line testing-library/no-render-in-setup
-        render(<App />);
-    });
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<App />);
+  });
 
-    test("Home page display app title", () => {
-        const appTitle = screen.getByText("College Jobs");
-        expect(appTitle).toBeInTheDocument();
-    });
+  test("Home page display app title", () => {
+    const appTitle = screen.getByText("College Jobs");
+    expect(appTitle).toBeInTheDocument();
+  });
 
-    test("Login button is present", () => {
-        const loginButton = screen.getByRole("button", { name: "Login" });
+  test("Login button is present", () => {
+    const loginButton = screen.getByRole("button", { name: "Login" });
 
-        expect(loginButton).toBeInTheDocument();
-    });
+    expect(loginButton).toBeInTheDocument();
+  });
 
-    test("At least one listing is present", () => {
-        const listings = screen.getAllByText("See More");
-        expect(listings.length).toBeGreaterThan(0);
-    });
+  test("At least one listing is present", () => {
+    const listings = screen.getAllByText("See More");
+    expect(listings.length).toBeGreaterThan(0);
+  });
+
+  test("CI intagration", () => {
+    const loginButton = screen.getByRole("button", { name: "test" });
+
+    expect(loginButton).toBeInTheDocument();
+  });
 });
