cb15e1120bfe048974f8586406acbc2ce485b86c
Nick Gauthier <ngauthier404@icloud.com>
2023-12-09 16:16:18 -0500

    Refactor monster homepage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,97 +1,52 @@
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { ListingView } from "../components/ListingView";
+import { Button } from "react-bootstrap";
 import { ProfileDropDownButton } from "../components/ProfileDropdown";
-import { JobListing } from "../data/job_listing";
 import { getDummyJobListings } from "../dummy/job_listing";
 import { auth } from "../firebaseConfig";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { JobList } from "../components/JobList";
+import { SearchAndFilter } from "../components/SearchAndFilter";
 
 export function HomePage(): JSX.Element {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filterLocation, setFilterLocation] = useState("");
-  const [filterType, setFilterType] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filterLocation, setFilterLocation] = useState("");
 
-  const dummyListings = getDummyJobListings()
-    .filter(
-      (listing) =>
-        listing.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        listing.title.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-    .filter(
-      (listing) =>
-        filterLocation === "" || listing.criteria[0].value === filterLocation
-    )
-    .filter(
-      (listing) => filterType === "" || listing.criteria[1].value === filterType
+    const dummyListings = getDummyJobListings()
+        .filter(
+            (listing) =>
+                listing.company
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase()) ||
+                listing.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .filter((listing) =>
+            listing.location
+                .toLowerCase()
+                .includes(filterLocation.toLowerCase())
+        );
+
+    const logdInUserID = -1;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    return (
+        <div>
+            <header>
+                <h1>College Jobs</h1>
+                <ProfileDropDownButton
+                    userID={logdInUserID}
+                ></ProfileDropDownButton>
+                <Button onClick={() => signInWithGoogle()}>Login</Button>
+            </header>
+
+            <SearchAndFilter
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                filterLocation={filterLocation}
+                setFilterLocation={setFilterLocation}
+            />
+
+            <JobList listings={dummyListings} />
+        </div>
     );
-
-  const logdInUserID = -1;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-
-  const getFilterOptions = (field: string): string[] => {
-    const options = new Set<string>();
-    getDummyJobListings().forEach((listing) => {
-      listing.criteria.forEach((criteria) => {
-        if (criteria.field === field) {
-          options.add(criteria.value);
-        }
-      });
-    });
-    return Array.from(options);
-  };
-
-  return (
-    <div>
-      <header>
-        <h1>College Jobs</h1>
-        <ProfileDropDownButton userID={logdInUserID}></ProfileDropDownButton>
-        <Button onClick={() => signInWithGoogle()}>Login</Button>
-      </header>
-
-      <div className="searchAndFilter">
-        <Form>
-          <Form.Control
-            type="text"
-            placeholder="Search by company or title"
-            value={searchTerm}
-            onChange={(event) => setSearchTerm(event.target.value)}
-          />
-          <Form.Control
-            as="select"
-            value={filterLocation}
-            onChange={(event) => setFilterLocation(event.target.value)}
-          >
-            <option value="">Filter by Location</option>
-            {getFilterOptions("Location").map((location) => (
-              <option key={location} value={location}>
-                {location}
-              </option>
-            ))}
-          </Form.Control>
-          <Form.Control
-            as="select"
-            value={filterType}
-            onChange={(event) => setFilterType(event.target.value)}
-          >
-            <option value="">Filter by Employment Type</option>
-            {getFilterOptions("Employment Type").map((type) => (
-              <option key={type} value={type}>
-                {type}
-              </option>
-            ))}
-          </Form.Control>
-        </Form>
-      </div>
-
-      <div className="feedBox">
-        {dummyListings.map(
-          (listing: JobListing): JSX.Element => (
-            <ListingView key={listing.id} listing={listing} />
-          )
-        )}
-      </div>
-    </div>
-  );
 }
