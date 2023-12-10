3b53c764c1989ebbb953c046b62e5cfeca546769
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-12-09 20:17:12 -0500

    add apply button and test for listing page

--- src/components/ListingView.tsx
+++ src/components/ListingView.tsx
@@ -3,22 +3,25 @@ import { JobListing } from "../data/job_listing";
 import { Button } from "react-bootstrap";
 
 export interface ListingViewProps {
-    listing: JobListing;
+  listing: JobListing;
 }
 
 export function ListingView({ listing }: ListingViewProps): JSX.Element {
-    return (
-        <div className="listing">
-            <h3>{listing.company}</h3>
-            <h4>{listing.title}</h4>
-            <p>
-                {listing.description.length > 150
-                    ? listing.description.substring(0, 147) + "..."
-                    : listing.description}
-            </p>
-            <Link to={"/listing/" + listing.id}>
-                <Button>See More</Button>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="listing">
+      <h3>{listing.company}</h3>
+      <h4>{listing.title}</h4>
+      <p>
+        {listing.description.length > 150
+          ? listing.description.substring(0, 147) + "..."
+          : listing.description}
+      </p>
+      <Link to={"/listing/" + listing.id}>
+        <Button>See More</Button>
+      </Link>
+      <Link to={listing.url}>
+        <Button>Apply</Button>
+      </Link>
+    </div>
+  );
 }
