881c20c4ee19f5184e1817441d57e6f40a7b4028
NSawyer <33187059+GShadow5@users.noreply.github.com>
2023-12-10 12:13:15 -0500

    Switched LoginButton to use RefreshProp from Header

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -6,62 +6,50 @@ import { auth, db } from "../firebaseConfig";
 import { collection } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { useNavigate } from "react-router-dom";
+import { RefreshProp } from "./Header";
 
-export interface LoginButtonProps {
-    refresh: boolean;
-    setRefresh: (refresh: boolean) => void;
-}
-
-export function LoginButton(refresh: LoginButtonProps): JSX.Element {
+export function LoginButton(prop: RefreshProp): JSX.Element {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [value, loading, error] = useCollection( collection(db, "User") );
     let navigate = useNavigate();
 
     const signInWithGoogle = () => {
-        setPersistence(auth, browserSessionPersistence)
-        .then(() => {
-            signInWithPopup(auth, provider).then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                if (credential === null) {
-                    throw new Error("credential is null");
-                }
-                //const token = credential.accessToken;
-                // The signed-in user info.
-                const gAuthUser = result.user;
-                // check if user exists in database
-                // if not, redirect to register page
-                
-                const firebaseUser = value?.docs.find((doc) => doc.data().email === gAuthUser.email);
-                if (firebaseUser === undefined) {
-                    console.log("user not found in database");
-                    // redirect to register page
-                    
-                    navigate("/register");
-                }
-                else {
-                    // redirect to home page
-                    console.log("user found in database");
-                    refresh.setRefresh(!refresh.refresh);
-                }
-                console.log(firebaseUser); // DEBUG
-                console.log("user signed in"); // DEBUG
-                console.log(gAuthUser); // DEBUG 
-                console.log(gAuthUser.email); // DEBUG
+        signInWithPopup(auth, provider).then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            if (credential === null) {
+                throw new Error("credential is null");
+            }
 
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-                console.log("error signing in"); // DEBUG 
-                console.log(errorCode, errorMessage, email, credential); // DEBUG
-            });
+            // The signed-in user info.
+            const gAuthUser = result.user;
+
+            // check if user exists in database
+            // if not, redirect to register page 
+            const firebaseUser = value?.docs.find((doc) => doc.data().email === gAuthUser.email);
+            if (firebaseUser === undefined) {
+                console.log("user not found in database");                    
+                navigate("/register");
+            }
+            else {
+                // refresh header if user is found in database
+                console.log("user found in database");
+                prop.setRefresh(!prop.refresh);
+            }
+
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+            console.log("error signing in"); // DEBUG 
+            console.log(errorCode, errorMessage, email, credential); // DEBUG
         });
     }
 
