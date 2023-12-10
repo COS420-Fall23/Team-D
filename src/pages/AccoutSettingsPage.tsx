308b23e2c4459f8047db0f99b87db2e992f6f70d
NSawyer <33187059+GShadow5@users.noreply.github.com>
2023-12-10 12:27:26 -0500

    Updated account settings page to use new user data system

--- src/pages/AccoutSettingsPage.tsx
+++ src/pages/AccoutSettingsPage.tsx
@@ -2,26 +2,38 @@ import React from "react";
 import { Button } from "react-bootstrap";
 //import { ProfileDropDownButton } from "../components/ProfileDropdown";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from "firebase/firestore";
+import { Query, QueryDocumentSnapshot, collection, doc, getDoc } from "firebase/firestore";
 import { auth, db } from "../firebaseConfig";
 import { Header } from "../components/Header";
 
 export function AccountSettingsPage(): JSX.Element {
-  const [value] = useCollection(collection(db, "User"));
+  const [user, setUser] = React.useState(null as QueryDocumentSnapshot | null);
 
-  const FireBaseUser = value?.docs.find(
-    (user): boolean => user.data().Email === auth.currentUser?.email
-  );
+  async function getUser() {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const docSnap = await getDoc(doc(db, "User", auth.currentUser?.email as string));
+    if (docSnap.exists()) {
+      setUser(docSnap);
+      console.log("Document data:", docSnap.data());
+    }
+    //setUser(docSnap.exists() ? docSnap : null);
+  }
+
+  getUser();
 
   return (
     <div>
       <Header></Header>
       <h1>Account Settings</h1>
-      <div data-testid="Email">Email {FireBaseUser?.data().Email} </div>
+      {/* <div data-testid="Email">Email {user?.data().Email} </div> */}
+      <div data-testid="Email">Email {user !== null ? user.data().email : "Loading..."} </div>
       <div>
         Password <Button>ResetPassword</Button>
       </div>
-      <div data-testid="Phone">Phone {FireBaseUser?.data().phoneNumber}</div>
+      {/* <div data-testid="Phone">Phone {user?.data().phoneNumber}</div> */}
+      <div data-testid="Phone">Phone {user !== null ? user.data().phoneNumber : "Loading..."}</div>
       <div>Site Filter</div>
       <div>Job Keywords</div>
     </div>
