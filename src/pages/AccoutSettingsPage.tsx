db15fbd8172b85e353381d5c0ba2e81afbf61ef8
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-12-09 21:08:18 -0500

    test form

--- src/pages/AccoutSettingsPage.tsx
+++ src/pages/AccoutSettingsPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { User } from "../data/userInterface";
 import { ProfileDropDownButton } from "../components/ProfileDropdown";
 import { getDummyUsers } from "../dummy/Users";
@@ -21,12 +21,16 @@ export function AccountSettingsPage(): JSX.Element {
       </header>
       <h1>Account Settings</h1>
       <div data-testid="Email">Email {logedInUser?.Email} </div>
-      <div>
-        Password <Button>ResetPassword</Button>
-      </div>
       <div data-testid="Phone">Phone {logedInUser?.phoneNumber}</div>
-      <div>Site Filter</div>
-      <div>Job Keywords</div>
+      <div data-testid="College">College {logedInUser?.College}</div>
+      <div data-testid="Location">Location {logedInUser?.Location}</div>
+      <Form>
+        <h2>Skills</h2>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Skills</Form.Label>
+          <Form.Control type="text" placeholder="Enter Skill" />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
