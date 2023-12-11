a6c92dcd7738f93a1a9224972bcfc9eb3fa0e155
sethlvaughn <slvaughn1@gmail.com>
2023-12-11 15:53:59 -0500

    Finished styles.

--- src/pages/AccoutSettingsPage.tsx
+++ src/pages/AccoutSettingsPage.tsx
@@ -17,43 +17,45 @@ export function AccountSettingsPage(): JSX.Element {
   return (
     <div className="accountSettings">
       <Header></Header>
-      <h1>Account Settings</h1>
-      <div data-testid="Email">
-        <div>Email: {localUser.email}</div>{" "}
+      <div className="accountSettings-Content">
+        <h1>Account Settings</h1>
+        <div data-testid="Email">
+          <div>Email: {localUser.email}</div>{" "}
+        </div>
+        <div data-testid="FullName">
+          <div>Full Name: {localUser.fullName}</div>
+          <EditFullName
+            userEmail={localUser.email}
+            refresh={refresh}
+            setRefresh={setRefresh}
+          ></EditFullName>
+        </div>
+        <div data-testid="Phone">
+          <div>Phone: {localUser.phoneNumber}</div>
+          <EditPhonenumber
+            userEmail={localUser.email}
+            refresh={refresh}
+            setRefresh={setRefresh}
+          ></EditPhonenumber>
+        </div>
+        <div data-testid="College">
+          <div>College: {localUser.college}</div>
+          <EditCollege
+            userEmail={localUser.email}
+            refresh={refresh}
+            setRefresh={setRefresh}
+          ></EditCollege>
+        </div>
+        <div data-testid="Location">
+          <div>Location: {localUser.location}</div>
+          <EditLocation
+            userEmail={localUser.email}
+            refresh={refresh}
+            setRefresh={setRefresh}
+          ></EditLocation>
+        </div>
+        {<Skills refresh={refresh} setRefresh={setRefresh}></Skills>}
       </div>
-      <div data-testid="FullName">
-        <div>Full Name: {localUser.fullName}</div>
-        <EditFullName
-          userEmail={localUser.email}
-          refresh={refresh}
-          setRefresh={setRefresh}
-        ></EditFullName>
-      </div>
-      <div data-testid="Phone">
-        <div>Phone: {localUser.phoneNumber}</div>
-        <EditPhonenumber
-          userEmail={localUser.email}
-          refresh={refresh}
-          setRefresh={setRefresh}
-        ></EditPhonenumber>
-      </div>
-      <div data-testid="College">
-        <div>College: {localUser.college}</div>
-        <EditCollege
-          userEmail={localUser.email}
-          refresh={refresh}
-          setRefresh={setRefresh}
-        ></EditCollege>
-      </div>
-      <div data-testid="Location">
-        <div>Location: {localUser.location}</div>
-        <EditLocation
-          userEmail={localUser.email}
-          refresh={refresh}
-          setRefresh={setRefresh}
-        ></EditLocation>
-      </div>
-      {<Skills refresh={refresh} setRefresh={setRefresh}></Skills>}
     </div>
   );
 }
