b1162ebde92de7ce6ea0cbb487a2c2a24449d438
NSawyer <33187059+GShadow5@users.noreply.github.com>
2023-12-11 10:26:52 -0500

    added a refresh to update job list after sign in/out

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { JobListing } from "../data/job_listing";
 import { ListingView } from "./ListingView";
+import { UserSingleton, waitForUser } from "../data/user";
+import { auth } from "../firebaseConfig";
 
 export function JobList({ listings }: { listings: JobListing[] }): JSX.Element {
   const [refresh, setRefresh] = useState(false);
+
+  waitForUser(UserSingleton.getInstance(), refresh, setRefresh, "JobList");
+
+  auth.onAuthStateChanged((user) => {
+    waitForUser(UserSingleton.getInstance(), refresh, setRefresh, "JobList");
+  });
+
   return (
     <div className="feedBox">
       {listings.map(
